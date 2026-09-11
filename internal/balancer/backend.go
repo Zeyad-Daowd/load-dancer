ca3a4f6965bb60db17a18ac2af6845bb20619416
Zeyad Daowd <zeyaddaowd@yahoo.com>
2026-09-11 14:27:45 +0300

    feat: add LB-to-backend timeouts and proxy error handling

--- internal/balancer/backend.go
+++ internal/balancer/backend.go
@@ -1,16 +1,30 @@
 package balancer
 
 import (
+	"log"
+	"net"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
 	"sync/atomic"
+	"time"
 )
 
 type BackendServer struct {
 	addr   *url.URL
 	proxy  *httputil.ReverseProxy
 	health atomic.Bool
+}
+
+var transport = http.Transport{
+	DialContext: (&net.Dialer{
+		Timeout:   2 * time.Second,
+		KeepAlive: 10 * time.Second,
+	}).DialContext,
+	ResponseHeaderTimeout: 5 * time.Second,
+	IdleConnTimeout:       10 * time.Second,
+	MaxIdleConns:          100,
+	MaxIdleConnsPerHost:   20,
 }
 
 func CreateBackendServer(urlStr string) (*BackendServer, error) {
@@ -20,6 +34,11 @@ func CreateBackendServer(urlStr string) (*BackendServer, error) {
 	}
 	// reverse proxy to forward requests
 	proxy := httputil.NewSingleHostReverseProxy(parsedURL)
+	proxy.Transport = &transport
+	proxy.ErrorHandler = func(w http.ResponseWriter, r *http.Request, err error) {
+		log.Printf("Error proxying request to backend server %s: %v", parsedURL.String(), err)
+		http.Error(w, "backend unavailable", http.StatusBadGateway)
+	}
 	server := &BackendServer{addr: parsedURL, proxy: proxy}
 	server.health.Store(true)
 	return server, nil
