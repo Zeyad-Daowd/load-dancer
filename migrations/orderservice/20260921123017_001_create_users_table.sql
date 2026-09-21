5450b9ecb9b46d1bbb460c882c9a24349ef82547
Zeyad Daowd <zeyaddaowd@yahoo.com>
2026-09-21 15:44:08 +0300

    fix: change password_hash from VARCHAR(255) to TEXT in users table

--- migrations/orderservice/20260921123017_001_create_users_table.sql
+++ migrations/orderservice/20260921123017_001_create_users_table.sql
@@ -2,7 +2,7 @@
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     username VARCHAR(50) NOT NULL UNIQUE,
-    password_hash VARCHAR(255) NOT NULL,
+    password_hash TEXT NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     role VARCHAR(20) NOT NULL,
     CONSTRAINT role_check
