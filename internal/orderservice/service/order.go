99cb75882987d6515b2a3ee1b5997d637ee3369e
Zeyad Daowd <zeyaddaowd@yahoo.com>
2026-09-21 22:13:13 +0300

    feat: implement OrderService with CreateOrder method

---
+++ internal/orderservice/service/order.go
@@ -0,0 +1,34 @@
+package orderservice
+
+import (
+	"context"
+
+	db "github.com/zeyad-daowd/load-dancer/internal/orderservice/db/generated"
+)
+
+type OrderService struct {
+	queries *db.Queries
+}
+
+type CreateOrderRequest struct {
+	UserID         int32
+	IdempotencyKey string
+}
+
+func NewOrderService(queries *db.Queries) *OrderService {
+	return &OrderService{
+		queries: queries,
+	}
+}
+
+func (s *OrderService) CreateOrder(ctx context.Context, req *CreateOrderRequest) (*db.Order, error) {
+	createdOrder, err := s.queries.AddOrder(ctx, db.AddOrderParams{
+		UserID:         req.UserID,
+		IdempotencyKey: req.IdempotencyKey,
+		TotalCents:     0,
+	})
+	if err != nil {
+		return nil, err
+	}
+	return &createdOrder, nil
+}
