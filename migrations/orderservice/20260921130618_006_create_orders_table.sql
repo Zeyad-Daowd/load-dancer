7b8cda54c70f67f650b90e00313740270d276ea1
Zeyad Daowd <zeyaddaowd@yahoo.com>
2026-09-21 17:53:16 +0300

    fix: fix orders table status & not null timestamp

--- migrations/orderservice/20260921130618_006_create_orders_table.sql
+++ migrations/orderservice/20260921130618_006_create_orders_table.sql
@@ -3,11 +3,11 @@ CREATE TABLE orders (
     id BIGSERIAL PRIMARY KEY,
     user_id INTEGER NOT NULL REFERENCES users(id),
     status TEXT NOT NULL,
-    created_at TIMESTAMP,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     idempotency_key TEXT NOT NULL UNIQUE,
     total_cents INTEGER NOT NULL,
     CONSTRAINT status_check
-        CHECK (status IN ('pending', 'completed', 'cancelled')),
+        CHECK (status IN ('started', 'pending', 'completed', 'cancelled')),
     CONSTRAINT total_cents_check
         CHECK (total_cents >= 0)
 );
