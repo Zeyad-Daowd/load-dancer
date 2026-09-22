cfceb4765ff2f529adee43b435f50d1e76015489
Zeyad Daowd <zeyaddaowd@yahoo.com>
2026-09-22 17:46:49 +0300

    feat: implement user creation and retrieval methods in OrderService

---
+++ internal/orderservice/service/user.go
@@ -0,0 +1,73 @@
+package orderservice
+
+import (
+	"context"
+	"fmt"
+
+	db "github.com/zeyad-daowd/load-dancer/internal/orderservice/db/generated"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type CreateUserRequest struct {
+	Username string
+	Password string
+	Role     string
+}
+type User struct {
+	ID       int32
+	Username string
+	Role     string
+}
+
+func HashPassword(password string) (string, error) {
+	// Convert password string to byte slice
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
+	return string(bytes), nil
+}
+
+func CheckPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
+
+func (s *OrderService) CreateUser(ctx context.Context, req *CreateUserRequest) (*User, error) {
+	if req.Role != "admin" && req.Role != "user" {
+		return nil, fmt.Errorf("Invalid User Role")
+	}
+
+	hashed, err := HashPassword(req.Password)
+	if err != nil {
+		return nil, err
+	}
+	user, err := s.queries.CreateUser(ctx, db.CreateUserParams{
+		Username:     req.Username,
+		PasswordHash: hashed,
+		Role:         req.Role,
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &User{
+		ID:       user.ID,
+		Username: user.Username,
+		Role:     user.Role,
+	}, nil
+}
+
+func (s *OrderService) GetUserByUsername(ctx context.Context, username string) (*User, error) {
+	user, err := s.queries.GetUserByUsername(ctx, username)
+	if err != nil {
+		return nil, err
+	}
+
+	return &User{
+		ID:       user.ID,
+		Username: user.Username,
+		Role:     user.Role,
+	}, nil
+}
