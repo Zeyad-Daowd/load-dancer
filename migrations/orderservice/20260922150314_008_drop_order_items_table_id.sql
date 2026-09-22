1358f8540bb26ad3c98ce064130f498384139682
Zeyad Daowd <zeyaddaowd@yahoo.com>
2026-09-22 18:08:36 +0300

    refactor: fix migration to single alter table

--- migrations/orderservice/20260922150314_008_drop_order_items_table_id.sql
+++ migrations/orderservice/20260922150314_008_drop_order_items_table_id.sql
@@ -1,24 +1,14 @@
 -- +goose Up
 ALTER TABLE order_items
-    DROP CONSTRAINT order_items_pkey;
-
-ALTER TABLE order_items
-    DROP COLUMN id;
-
-ALTER TABLE order_items
-    DROP CONSTRAINT order_product_unique;
-
-ALTER TABLE order_items
+    DROP CONSTRAINT order_items_pkey,
+    DROP COLUMN id,
+    DROP CONSTRAINT order_product_unique,
     ADD CONSTRAINT order_items_pkey
         PRIMARY KEY (order_id, product_id);
 
 -- +goose Down
 ALTER TABLE order_items
-    DROP CONSTRAINT order_items_pkey;
-
-ALTER TABLE order_items
-    ADD COLUMN id SERIAL PRIMARY KEY;
-
-ALTER TABLE order_items
+    DROP CONSTRAINT order_items_pkey,
+    ADD COLUMN id SERIAL PRIMARY KEY,
     ADD CONSTRAINT order_product_unique
         UNIQUE (order_id, product_id);
